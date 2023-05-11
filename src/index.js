0ba5225120ee45760d43b4bdf31e5bac30738545
antoniomauricio16 <antoniomauriciotecg@gmail.com>
2023-05-11 20:52:42 -0300

    criacao de usuario

--- src/index.js
+++ src/index.js
@@ -10,3 +10,66 @@ return response.json('OK');
 
 app.listen(8080, () => console.log('Servidor iniciado'));
 
+//lista de usuarios
+let users = [];
+
+app.get(`/user`, (request, response) => {
+  return response.json(users);
+});
+//criação de usuario
+app.post(`/user`, (request, response) => {
+  const user = request.body;
+  const saltRounds = 10;
+    
+  bcrypt.hash(user.senha, saltRounds, function(err, hash) {
+    if(hash){
+        users.push({
+            id: Math.floor(Math.random()*67676),
+            nome: user.nome,
+            email: user.email,
+            senha: hash
+        }); 
+        const dados = request.body;
+const novoUsuario = {
+  id: Math.floor(Math.random()*67676),
+  nome: dados.nome,
+  email: dados.email,
+  senha: dados.email,
+};
+const existe = users.some((users) => users.email === novoUsuario.email );
+if (existe) {
+  return response.status(400).json({
+    sucesso: false,
+    dados: null,
+    mensagem: "Outro usuário já está cadastrado com este e-mail.",
+  });
+}
+users.push({
+  id: Math.floor(Math.random() * 67676),
+  nome: user.nome,
+  email: user.email,
+ senha: hash
+});
+
+if (
+  !user.email ||
+  !user.email.includes("@") ||
+  !user.email.includes(".com")
+) {
+  return response.status(400).json({
+    sucesso: false,
+    dados: null,
+    mensagem:
+      "É obrigatório informar um e-mail válido para cadastro do usuário",
+  });
+}
+
+return response.status(201).json();
+       
+    } else {
+        return response.status(400).json("Ocorreu um erro:" + err)
+    }
+  });
+});
+
+
