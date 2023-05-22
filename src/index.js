82c695e185bd01a28aca7905d83a267b258eb90b
antoniomauricio16 <antoniomauriciotecg@gmail.com>
2023-05-22 20:15:33 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
+
+app.options('*', cors()) // include before other routes
 import express from 'express';
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
 
 import bcrypt, { hash } from "bcrypt";
 const app = express();
