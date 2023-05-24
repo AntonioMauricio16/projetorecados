ec2cc90dfde32bb7ba6fa593384dc190aeea7f60
antoniomauricio16 <antoniomauriciotecg@gmail.com>
2023-05-24 20:16:53 -0300

    atualizando o cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-const express = require('express');
-import express, { response } from 'express';
-import bcrypt, { hash } from "bcrypt";
-const app = express();
-app.use(express.json());
-var cors = require('cors')
-app.use(cors("*"))
+import  express, { request } from "express";
+import cors from "cors";
 
+const app = express();
+const bcrypt = require('bcrypt');
+app.use(express.json());
+app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 
 app.get('/', (request, response) => {
@@ -17,159 +21,152 @@ return response.json('OK');
 app.listen(8080, () => console.log('Servidor iniciado'));
 
 
-//lista de usuarios
-let users = [];
+let usuarios = [];
 
-app.get(`/user`, (request, response) => {
-  return response.json(users);
-});
-//criação de usuario
-app.post(`/user`, (request, response) => {
-  const user = request.body;
-  const saltRounds = 10;
-  const existe = users.some((users) => users.email === user.email );
-  if (existe) {
-    return response.status(400).json({
-      sucesso: false,
-      dados: null,
-      mensagem: "Outro usuário já está cadastrado com este e-mail.",
-    });
-    
+
+//esse e nosso midlaware que valida se tem id no array usuario para criar um recado //
+function validaPeloId(request, response, next) {
+  const usuarioId = request.params.id;
+  const usuarioValidado = usuarios.find(usuario => usuario.id === Number(usuarioId));
+
+  if (usuarioValidado) {
+    next(); // Se o usuário for encontrado, chama o próximo middleware ou rota
+  } else {
+    response.status(404).send("Usuário não encontrado");
   }
-  
-  bcrypt.hash(user.senha, saltRounds, function(err, hash) {
-    if(hash){
-        users.push({
-            id: Math.floor(Math.random()*67676),
-            nome: user.nome,
-            email: user.email,
-            senha: hash
-        }); 
-      }  
+}
 
+//ele vai criar nosso usuario// utilizando metodo post //
+  app.post('/usuarios',async (request,response)=>{
+      let { nome,email,senha}= request.body;
+      let validasetemusuariocadastrado = usuarios.find(user => user.email === email)
+      if(validasetemusuariocadastrado){
+          response.status(409).send("usuario ja existente")
+      }else{
+          let id = Math.floor(Math.random()*6767);
+          let senhaCriptografada = await bcrypt.hash(senha, 10);
+          let novoUsuario = {id, nome, email, senhaCriptografada, recado:[]};
+          usuarios.push(novoUsuario);
+          response.status(201).send("criado com sucesso");
+      }
   });
-  return response.status(400).json({
-    sucesso: false,
-    dados: null,
-    mensagem: " Usuário  cadastrado!.",
-  });
 
-});
-//criação d login
-
-app.post(`/login`, (request, response) => {
-    const dadosDoUsuario = request.body;
-  
-    const emailCorreto = users.some(
-      (user) => user.email === dadosDoUsuario.email
-    );
-  
-    const senhaCorreta = users.some(
-      (user) => user.password === dadosDoUsuario.password
-    );
-  
-    if (!emailCorreto || !senhaCorreta) {
-      return response.status(400).json({
-        success: false,
-        message: `Email ou senha estão incorretos`,
-        data: {},
-      });
+//criando login // utilizando metodo post //
+  app.post('/usuarios/login', async (request, response) => {
+    let { email, senha } = request.body;
+    let emailVerificado = usuarios.find(user => user.email === email);
+    
+    if (!emailVerificado) {
+      return response.status(401).send('Email ou senha inválidos');
     }
-  
-    users.forEach((usuario) => (usuario.logado = false));
-  
-    const user = users.find((user) => user.email === dadosDoUsuario.email);
-  
-    user.logado = true;
-  
-    return response.json({
-      success: true,
-      message: `Usuário logado com sucesso`,
-      data: {},
-    });
+    
+    let senhaComparada = await bcrypt.compare(senha, emailVerificado.senhaCriptografada);
+    
+    if (!senhaComparada) {
+      return response.status(401).send('Email ou senha inválidos');
+    }else{ 
+      response.status(202).json(emailVerificado.id)
+    }
   });
-  
-// criação de recados
-const listaRecados = [];
 
-app.get(`/list`, (request, response) => {
-  return response.json(listaRecados);
-});
+//vamos ler quem ta dentro do array // utilizando metodo get //
+  app.get('/usuarios', (request,response)=>{
+    response.status(202).json(usuarios);
+  })
 
-app.post(`/recados`, (request, response) => {
-  const dados = request.body;
+//criamos os recados no array de usuarios// utilizando metodo post //
+  app.post("/usuarios/:id/recado",validaPeloId, (request,response)=>{
+      const novoRecado = request.body;
+      let recadoCriado = {
+          id: Math.floor(Math.random()*1425),
+          titulo: novoRecado.titulo,
+          descricao: novoRecado.descricao
+      };
+      const id = request.params.id;
 
-  const usuario = users.find((user) => user.logado === true);
+      let pegueiIdParaRecado = usuarios.findIndex(usuario=> usuario.id === Number(id));
+
+      usuarios[pegueiIdParaRecado].recado.push(recadoCriado);
+
+    return response.status(201).send("Recado criado com sucesso")
+  });
+// vou listar so o recado do usuario selecionado
+
+app.get("/usuarios/:id/recado", (req, res) => {
+  const usuarioId = parseInt(req.params.id);
+
+  const usuario = usuarios.find((user) => user.id === usuarioId);
 
   if (!usuario) {
-    return response.status(400).json({
-      success: false,
-      message: `Necessario fazer login para criar um post`,
-      data: {},
-    });
+    return res.status(404).json("Usuário não encontrado.");
   }
 
-  //Fazer validacao dos dados do recado
+  const page = req.query.page || 1;
+  const pages = Math.ceil(usuario.recado?.length / 3);
+  const indice = (page - 1) * 3;
+  const aux = [...usuario.recado]; // spread operator
+  const result = aux.splice(indice, 3);
 
-  const novoRecado = {
-    id: new Date().getTime(),
-    titulo: dados.titulo,
-    descricao: dados.descricao,
-    autor: usuario,
-  };
-
-  listaRecados.push(novoRecado);
-  return response.status(201).json({
-    success: true,
-    message: `Recado criado com sucesso`,
-    data: novoRecado,
-  });
+  return res
+    .status(201)
+    .json({ total: usuario.recado.length, recados: result, pages });
 });
 
-/*Atualizacao dos recados*/
-app.get(`/atualizar/:id`, (request, response) => {
-  return response.json(recadoAtualizado);
-});
-const recadoAtualizado = [];
-app.put('/atualizar/:id', (request, response) => {
+
+//vamos alterar o recado do usuario // utilizando metodo o put //
+  app.put('/usuarios/:id/recado/:recadoId',validaPeloId,(req, res) => {
+      const usuarioId = req.params.id; // obtém o ID do usuário a partir da URL
+      const recadoId = req.params.recadoId; // obtém o ID do recado a partir da URL
     
-  
-    const { id } = request.params; 
-    const { titulo, descricao } = request.body 
-    const userIndex = recadoAtualizado.findIndex(user => user.id === id);
-  
-  
-    if(userIndex > 0){
-        return response.status(400).json({ error: 'User not found'});
-    }
-  
-    const user = {
-        id,
-        titulo,
-        descricao,
-        
-    };
-  recadoAtualizado[userIndex] = user;
-  recadoAtualizado.push(user);
+      // Encontra o usuário pelo ID
+      const usuario = usuarios.find(user => user.id === Number(usuarioId));
+
+      // Encontra o recado pelo ID
       
-  return response.json(user);
-  });
+      const recado = usuario.recado.find(recado => recado.id === Number(recadoId));
+    
+      
+      if (!recado ) {
+        return res.status(404).send('Recado não encontrado');
+      }
+    
+      if (usuario === undefined && recado === recado) {
+        return res.status(404).send('Usuario não encontrado');
+      }
+    
+      // Atualiza o recado
+      recado.titulo = req.body.titulo;
+      recado.descricao = req.body.descricao;
+    
+      res.status(200).send('Recado atualizado com sucesso');
+    });
 
-  //deletar recados
-app.delete(`/recados/:id`, (request, response) => {
-    const params = request.params;
-  
-    const recadoExiste = listaRecados.findIndex(
-      (recado) => recado.id === params.id
-    );
-  
-    if (recadoExiste > 0) {
-      return response.status(400).json(`Recado nao encontrado`);
-    }
-  
-    listaRecados.splice(recadoExiste, 1);
-  
-    return response.json(`Recado deletado`);
+// excluindo o recado por ID // utilizando metodo delete //
+  app.delete('/usuarios/:id/recado/:idRecado', (request, response)=>{
+    const id = Number(request.params.id);
+    const idRecado = Number(request.params.idRecado);
+    const indexDoUsuario = usuarios.findIndex(usuario => usuario.id === id);
+
+    //verifica se tem o id no array usuarios
+        if (indexDoUsuario < 0) {
+        response.status(404).send("Usuário não encontrado.");
+        return;
+        }
+
+    //pega o index do array usuarios
+    const indexDoRecado = usuarios[indexDoUsuario].recado.findIndex(recado => recado.id === idRecado);
+
+    // verifica se tem o id no array recado
+        if (indexDoRecado < 0) {
+        response.status(404).send("Recado não encontrado.");
+        return;
+        }
+
+    //pega o id do array recado no array usuarios e exclui    
+    usuarios[indexDoUsuario].recado.splice(indexDoRecado, 1);
+    response.status(202).send("Recado excluído com sucesso.");
   });
   
-
+  app.listen(1425, ()=>{
+      console.log("servidor rodando")
+  });
