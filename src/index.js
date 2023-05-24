af21f33a703c003e44683997e4f4fc2984f18bf2
antoniomauricio16 <antoniomauriciotecg@gmail.com>
2023-05-24 11:38:45 -0300

    corrigendo validação  do email

--- src/index.js
+++ src/index.js
@@ -33,7 +33,16 @@ app.get(`/user`, (request, response) => {
 app.post(`/user`, (request, response) => {
   const user = request.body;
   const saltRounds = 10;
+  const existe = users.some((users) => users.email === user.email );
+  if (existe) {
+    return response.status(400).json({
+      sucesso: false,
+      dados: null,
+      mensagem: "Outro usuário já está cadastrado com este e-mail.",
+    });
     
+  }
+  
   bcrypt.hash(user.senha, saltRounds, function(err, hash) {
     if(hash){
         users.push({
@@ -42,49 +51,10 @@ app.post(`/user`, (request, response) => {
             email: user.email,
             senha: hash
         }); 
-        const dados = request.body;
-const novoUsuario = {
-  id: Math.floor(Math.random()*67676),
-  nome: dados.nome,
-  email: dados.email,
-  senha: dados.email,
-};
+      }  
 
-users.push({
-  id: Math.floor(Math.random() * 67676),
-  nome: user.nome,
-  email: user.email,
- senha: hash
-});
-const existe = users.some((users) => users.email === novoUsuario.email );
-if (existe) {
-  return response.status(400).json({
-    sucesso: false,
-    dados: null,
-    mensagem: "Outro usuário já está cadastrado com este e-mail.",
   });
-}
 
-if (
-  !user.email ||
-  !user.email.includes("@") ||
-  !user.email.includes(".com")
-) {
-  return response.status(400).json({
-    sucesso: false,
-    dados: null,
-    mensagem:
-      "É obrigatório informar um e-mail válido para cadastro do usuário",
-  });
-}
-
-return response.status(201).json();
-       
-    } else {
-        return response.status(400).json("Ocorreu um erro:" + err)
-    }
-  });
-});
 
 
 //criação d login
@@ -204,4 +174,4 @@ app.delete(`/recados/:id`, (request, response) => {
     return response.json(`Recado deletado`);
   })
   
-
+});
